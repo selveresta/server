282367bc02f39e488ddda24d27a2725b513b01f7
Artem <artem.lypnyk@infotech.gov.ua>
2025-01-18 22:01:05 +0200

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { StreamModule } from './modules/stream/stream.module';
+import { DatabaseModule } from './src/modules/database/database.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-	imports: [],
+	imports: [UserModule, StreamModule, DatabaseModule],
 	controllers: [],
 	providers: [],
 })
