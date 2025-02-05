54aff9c1e8840f2a43e5ced29fe406a0d5849192
Artem <artem.lypnyk@infotech.gov.ua>
2025-02-05 15:47:13 +0200

    add sequilize connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfigService } from '@C/sequelizeConfig.service';
 import { Config } from '@C/configuration';
-
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			load: [Config],
+		}),
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			useClass: SequelizeConfigService,
 		}),
 	],
 	controllers: [],
